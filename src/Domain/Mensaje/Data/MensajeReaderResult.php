e040eadec0a7b02c4020cccc5eb7603e922cedf8
elychirivella10 <elychirivell10@gmail.com>
2024-09-08 01:13:56 +0000

    cambios

--- src/Domain/Mensaje/Data/MensajeReaderResult.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Domain\Mensaje\Data;
-
-final class MensajeReaderResult
-{
-    public ?string $area_name = null;
-    public ?int $val = null;
-}
--- src/Domain/Requerimientos/Data/RequerimientosbyEstadosFinderItem.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Domain\Requerimientos\Data;
-
-final class RequerimientosbyEstadosFinderItem
-{
-    public ?string $estado = null;
-    public ?int $total = null;
-}
